09cef529fe46803401ae8f1cea4000a94ca1817f
rameez <rameez@newagesmb.com>
2022-12-24 15:00:23 +0530

    ok

---
+++ backend/data/products.js
@@ -0,0 +1,81 @@
+const products = [
+  {
+    name: "Airpods Wireless Bluetooth Headphones",
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/kpinwy80/headphone/i/t/k/mrxj2hn-a-apple-original-imag3qe9sub2tnzj.jpeg?q=70",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 16999,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "iPhone 11 Pro 256GB Memory",
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/z/a/f/iphone-11-pro-max-256-u-mwhm2hn-a-apple-0-original-imafkg2ftc5cze5n.jpeg?q=70",
+    description:
+      "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+    brand: "Apple",
+    category: "Electronics",
+    price: 64900,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: "Cannon EOS 80D DSLR Camera",
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/ksdjma80/dslr-camera/y/d/q/eos-80d-eos-80d-dslr-camera-body-with-single-lens-ef-s-18-135-is-original-imag5ymvhqjjbhsp.jpeg?q=70",
+    description:
+      "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 91592,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: "Sony Playstation 5 825 GB with Astro's Playroom (White)",
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/kj7gwi80/gamingconsole/t/v/v/cfi-1008a01r-825-sony-no-original-imafytxe7twjskbx.jpeg?q=70",
+    description:
+      "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
+    brand: "Sony",
+    category: "Electronics",
+    price: 49990,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: "Logitech G-Series Gaming Mouse",
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/kkwwu4w0/mouse/2/h/g/g402-logitech-original-imagy5ny25h8nsms.jpeg?q=70",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 2195,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: "Echo Dot (3rd gen) ",
+    image: "https://m.media-amazon.com/images/I/61EXU8BuGZL._SL1100_.jpg",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 3099,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
